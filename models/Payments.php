0e44ac4a5b6341559f3b965ef713f05152dbe2db
Tom K <tomtomklima@users.noreply.github.com>
2016-07-08 21:34:44 +0200

    added sending copy of new invoice mail

--- models/Payments.php
+++ models/Payments.php
@@ -192,9 +192,19 @@ class Payments extends Model {
 		
 		//send email to user
 		$subject = NAME.' Paralelní Polis - nová faktura';
-		$link = '<a href="'.ROOT.'">'.ROOT.'</a>';
-		$message = 'Zdravím! '.NAME.' vygeneroval novou fakturu na toto období. Klikem na '.$link.' ji můžete zaplatit.';
+		$link = ROOT.'/cs/payments';
+		$message = 'Ahoj,
+
+vystavili jsem ti fakturu za členství / pronájem v Paper Hub v Paralelní Polis.
+<a href="'.$link.'">'.$link.'</a>
+Platbu uhradíš jednoduše na odkazu výše. 
+
+Díky za rychlou platbu!
+Paper Hub';
 		$this->sendEmail(EMAIL, $user['email'], $subject, $message);
+		//and send copy of email to hub manager
+		//TODO refractor
+		$this->sendEmail(EMAIL, 'hub@paralelnipolis.cz', NAME.' - Poslána výzva o nové faktuře na email '.$user['email'], $message);
 		
 		return ['s' => 'success'];
 	}
